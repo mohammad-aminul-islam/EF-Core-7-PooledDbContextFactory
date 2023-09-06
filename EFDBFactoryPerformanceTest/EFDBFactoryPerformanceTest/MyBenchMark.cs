87860b23b47eb36293fd4489c0c8998c542d3a19
Mohammad Aminul <aminul.cseruet@gmail.com>
2023-09-07 01:21:51 +0600

    EF core update analysis

--- EFDBFactoryPerformanceTest/EFDBFactoryPerformanceTest/MyBenchMark.cs
+++ EFDBFactoryPerformanceTest/EFDBFactoryPerformanceTest/MyBenchMark.cs
@@ -20,24 +20,47 @@ namespace EFDBFactoryPerformanceTest
         }
 
         [Benchmark]
-        public void Get_With_Single_Factory_Instance()
+        public void Update_with_get_SaveChange()
         {
-            var dbcontext = factory.CreateDbContext();
-            var data = dbcontext.Logs.ToList();
-            dbcontext.Dispose();
+            using var dbcontext = factory.CreateDbContext();
+            dbcontext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+            var log = dbcontext.Logs.FirstOrDefault(x => x.Id == 1);
+            log.Message = "Updating from get and save change";
+            log.TimeStamp = DateTime.Now;
+            dbcontext.SaveChanges();
         }
 
         [Benchmark]
-        public void Get_With_Multiple_Factory_Instance()
+        public void Update_with_ExecuteUpdate()
         {
-            var options = new DbContextOptionsBuilder<AppDBContext>()
-                        .UseSqlServer(@"Server=localhost;Database=SERILOG;Trusted_Connection=True;Encrypt=false")
-                        .Options;
-
-            var poolFactory = new PooledDbContextFactory<AppDBContext>(options);
-            var dbcontext = poolFactory.CreateDbContext();
-            var data = dbcontext.Logs.ToList();
-            dbcontext.Dispose();
+            using var dbcontext = factory.CreateDbContext();
+            dbcontext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+            var log = dbcontext.Logs
+                .TagWith("Execute update testing")
+                .Where(d => d.Id == 1)
+                .ExecuteUpdate(x => x
+                .SetProperty(p => p.Message, "Execute update")
+                .SetProperty(p => p.TimeStamp, DateTime.Now)
+                );
         }
+
+        //[Benchmark]
+        //public void Get_With_Single_Factory_Instance()
+        //{
+        //    using var dbcontext = factory.CreateDbContext();
+        //    var data = dbcontext.Logs.ToList();
+        //}
+
+        //[Benchmark]
+        //public void Get_With_Multiple_Factory_Instance()
+        //{
+        //    var options = new DbContextOptionsBuilder<AppDBContext>()
+        //                .UseSqlServer(@"Server=localhost;Database=SERILOG;Trusted_Connection=True;Encrypt=false")
+        //                .Options;
+
+        //    var poolFactory = new PooledDbContextFactory<AppDBContext>(options);
+        //    using var dbcontext = poolFactory.CreateDbContext();
+        //    var data = dbcontext.Logs.ToList();
+        //}
     }
 }
--- EFDBFactoryPerformanceTest/EFDBFactoryPerformanceTest/Program.cs
+++ EFDBFactoryPerformanceTest/EFDBFactoryPerformanceTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BenchmarkDotNet.Running;
 using EFDBFactoryPerformanceTest;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 BenchmarkRunner.Run<MyBenchMark>();
